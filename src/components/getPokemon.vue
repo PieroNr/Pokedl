501d87cdffacc33f333ee62a56ce510d442d7b72
Piero <piero.neri@hotmail.fr>
2023-12-19 23:10:42 +0100

    fix for deploy

--- src/components/getPokemon.vue
+++ src/components/getPokemon.vue
@@ -26,9 +26,13 @@
       <div v-for="triedPokemon in triedPokemons" :key="triedPokemon.name" class="try">
         <img class="spriteList" v-if="triedPokemon && triedPokemon.spriteUrl" :src="triedPokemon.spriteUrl"/>
         <span :style="{color: triedPokemon.firstType.name == pokemon?.firstType.name ? 'green' : 'red'}">{{ triedPokemon.firstType.name }}</span>
-        <span :style="{color: (triedPokemon.secondType && pokemon?.secondType && triedPokemon.secondType.name == pokemon?.secondType.name) || (!triedPokemon.secondType && !pokemon?.secondType) ? 'green' : 'red'}">{{ triedPokemon.secondType ? triedPokemon.secondType.name : 'Auncun' }}</span>
+        <span :style="{color: (triedPokemon.secondType && pokemon?.secondType && triedPokemon.secondType.name == pokemon?.secondType.name) || (!triedPokemon.secondType && !pokemon?.secondType) ? 'green' : 'red'}">{{ triedPokemon.secondType ? triedPokemon.secondType.name : 'Aucun' }}</span>
         <span :style="{color: triedPokemon.color == pokemon?.color ? 'green' : 'red'}">{{ triedPokemon.color }}</span>
         <span :style="{color: (triedPokemon.habitation && triedPokemon.habitation == pokemon?.habitation) || (!triedPokemon.habitation && !pokemon?.habitation) ? 'green' : 'red'}">{{ triedPokemon.habitation ? triedPokemon.habitation : '?' }}</span>
+        <span :style="{color: triedPokemon.generation == pokemon?.generation ? 'green' : 'red'}">{{ triedPokemon.generation }}</span>
+        <span :style="{color: triedPokemon.height == pokemon?.height ? 'green' : 'red'}">{{ triedPokemon.height < pokemon?.height ? triedPokemon.height + 'm⇧' : triedPokemon.height > pokemon?.height ? triedPokemon.height + 'm⇩' : triedPokemon.height +'m' }}</span>
+        <span :style="{color: triedPokemon.height == pokemon?.height ? 'green' : 'red'}">{{ triedPokemon.weight < pokemon?.weight ? triedPokemon.weight + 'kg⇧' : triedPokemon.weight > pokemon?.weight ? triedPokemon.weight + 'kg⇩' : triedPokemon.weight +'kg' }}</span>
+
       </div>
 
 
--- src/services/SupabaseService.ts
+++ src/services/SupabaseService.ts
@@ -6,8 +6,8 @@ class SupabaseService {
 
 
     getClient() {
-        const supabaseUrl = process.env.VITE_SUPABASE_URL || '';
-        const supabaseKey = process.env.VITE_SUPABASE_KEY || '';
+        const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || '';
+        const supabaseKey = import.meta.env.VITE_SUPABASE_KEY || '';
         const supabase = createClient(supabaseUrl, supabaseKey);
         return supabase;
     }
