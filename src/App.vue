75a932a71ca63f8a507896abca8e49aca3382354
pneri <piero.neri@pole-emploi.fr>
2023-12-22 17:06:44 +0100

    test push notification

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,35 @@
-<script setup lang="ts">
+<script lang="ts">
+import { defineComponent } from "vue";
 
+export default defineComponent({
+  name: "App",
+  setup() {
+    return {};
+  },
+  methods: {
+    enableNotifications() {
+      if (!("Notification" in window)) {
+        alert("This browser does not support desktop notification");
+      } else if (Notification.permission === "granted") {
+        const notification = new Notification("Hi already!");
+      } else if (Notification.permission !== "denied") {
+        Notification.requestPermission().then(function (permission) {
+          if (permission === "granted") {
+            const notification = new Notification("Hi there!");          }
+        });
+      }
+    },
+
+
+  },
+});
 </script>
 
 <template>
 <div class="container mt-3">
+      <button id="notifications"
+              @click="enableNotifications"
+      >Enable notifications</button>
       <router-view />
     </div>
 </template>
