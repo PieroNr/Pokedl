5faeb08eaf85031ae241b1c20f41994b154f4a30
pneri <piero.neri@pole-emploi.fr>
2023-12-22 17:08:05 +0100

    fix for deploy

--- src/App.vue
+++ src/App.vue
@@ -11,11 +11,11 @@ export default defineComponent({
       if (!("Notification" in window)) {
         alert("This browser does not support desktop notification");
       } else if (Notification.permission === "granted") {
-        const notification = new Notification("Hi already!");
+         new Notification("Hi already!");
       } else if (Notification.permission !== "denied") {
         Notification.requestPermission().then(function (permission) {
           if (permission === "granted") {
-            const notification = new Notification("Hi there!");          }
+            new Notification("Hi there!");          }
         });
       }
     },
